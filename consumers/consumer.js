e183b2197b0a0980ceba2aa39b0d46a0c4dd8c49
Mohamed DIAG <Mohamed.DIAG.dz@viacesi.fr>
2025-06-03 22:44:38 +0200

    consumer

---
+++ consumers/consumer.js
@@ -0,0 +1,18 @@
+import { connectRabbitMQ } from '../common/connect.js'
+
+const queue = 'result_queue';
+(async () => {
+    const channel = await connectRabbitMQ();
+
+    await channel.assertQueue(queue, { durable: false });
+    console.log('Consumer listening for messages on queue:', queue);
+
+    channel.consume(queue, (msg) => {
+        if (msg !== null) {
+            const payload = JSON.parse(msg.content.toString());
+            console.log('Received result:', payload);
+            channel.ack(msg); // Acknowledge the message
+            console.log('Processing result for payload:', payload);
+        }
+    });
+})();
